a7434b596d1074adcd8a39aaa667e2f18d6aee0b
Shubham Kumar <shubhamshandilya198@gmail.com>
2020-01-20 20:24:13 +0530

    Initial commit

---
+++ app/src/main/java/shubhamshandilya/com/srma/MainActivity.java
@@ -0,0 +1,13 @@
+package shubhamshandilya.com.srma;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
